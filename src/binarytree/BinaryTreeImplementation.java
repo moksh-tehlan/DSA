dd43fa89daf9f0ad0b5842b307e2d955abe7d90a
Moksh Tehlan <tehlanmoksh@gmail.com>
2025-03-18 20:37:12 +0530

    feat: Binary Tree

---
+++ src/binarytree/BinaryTreeImplementation.java
@@ -0,0 +1,9 @@
+package binarytree;
+
+public class BinaryTreeImplementation {
+    public static void main(String[] args) {
+        TreeNode bt = new TreeNode(5);
+        bt.left = new TreeNode(6);
+        bt.right = new TreeNode(7);
+    }
+}
---
+++ src/binarytree/TreeNode.java
@@ -0,0 +1,13 @@
+package binarytree;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        left = null;
+        right = null;
+        this.data = data;
+    }
+}
